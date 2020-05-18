739719411ca8bfe2fadb86d1bbe12d5dd680377a
Mark Lewis <mlewis@trinity.edu>
2020-05-18 17:07:29 -0500

    Got this working on an OpenCL device.

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -4,26 +4,27 @@ import com.aparapi._
 import com.aparapi.device.Device
 import com.aparapi.device.OpenCLDevice
 
+class MyKernel(inA: Array[Double], inB: Array[Double], result: Array[Double]) extends Kernel {
+  override def run(): Unit = {
+      val i = getGlobalId()
+      result(i) = inA(i) + inB(i)
+  }
+}
+
 object Hello extends Greeting {
   val inA = Array.fill(10000)(math.random)
   val inB = inA.map(1.0 - _)
   val result = inA.map(_ => 0.0)
-
-  val kernel = new Kernel() {
-    override def run(): Unit = {
-        val i = getGlobalId()
-        result(i) = inA(i) + inB(i)
-    }
-  }
 
   def main(args: Array[String]): Unit = {
     val devices = OpenCLDevice.listDevices(null)
     println(devices)
     println(greeting)
     val range = Range.create(result.length);
+    val kernel = new MyKernel(inA, inB, result)
     kernel.execute(range);
     Thread.sleep(100)
-    println(result.take(10).mkString(", "))
+    println(result.takeRight(10).mkString(", "))
   }
 }
 
